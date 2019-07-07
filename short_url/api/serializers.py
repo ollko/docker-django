ac39505b19c6d17643ad7ac694513ba59bb2e9be
ollko <korotkaya.olga@yandex.ru>
2019-07-07 18:02:44 +0300

    Fix validation

--- short_url/api/serializers.py
+++ short_url/api/serializers.py
@@ -1,10 +1,18 @@
 
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 from short_url.models import Url
 
-class UrlSerializer(serializers.ModelSerializer):
 
-    class Meta:
-        model = Url
-        fields = ('user_id', 'long_url', 'short_url_subpart', 'created')
+class UrlSerializer(serializers.Serializer):
+    user_id = serializers.CharField(max_length=16)
+    long_url = serializers.URLField(max_length=200)
+    short_url_subpart = serializers.SlugField(
+        allow_blank=True, allow_unicode=False, max_length=50,
+        required=False, validators=[UniqueValidator(queryset=Url.objects.all())])
+    created = serializers.DateTimeField(read_only=True)
+
+
+    def create(self, validated_data):
+        return Url(**validated_data)
