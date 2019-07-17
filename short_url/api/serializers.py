f88854d4bc7986e092a81e738b6a2b952d8d94e1
ollko <korotkaya.olga@yandex.ru>
2019-07-17 18:25:22 +0300

    Change form's error messages

--- short_url/api/serializers.py
+++ short_url/api/serializers.py
@@ -1,6 +1,6 @@
 
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 
 from short_url.models import Url
 
@@ -9,10 +9,22 @@ class UrlSerializer(serializers.Serializer):
     user_id = serializers.CharField(max_length=16)
     long_url = serializers.URLField(max_length=200)
     short_url_subpart = serializers.SlugField(
-        allow_blank=True, allow_unicode=False, max_length=50,
-        required=False, validators=[UniqueValidator(queryset=Url.objects.all())])
+        allow_blank=True,
+        allow_unicode=False,
+        max_length=50,
+        required=False,
+        validators=[UniqueValidator(queryset=Url.objects.all(),
+        message='Этот subpart уже занят!')])
     created = serializers.DateTimeField(read_only=True)
 
+    class Meta:
+        validators = [
+            UniqueTogetherValidator(
+                queryset = Url.objects.all(),
+                fields = ['user_id', 'long_url'],
+                message = 'У вас уже есть короткая ссылка для этого url !'
+            ),
+        ]
 
     def create(self, validated_data):
         return Url.objects.create(**validated_data)
