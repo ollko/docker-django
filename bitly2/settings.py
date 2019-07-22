cf3734508736197f9499d88e02dd5c713a21b3d3
ollko <korotkaya.olga@yandex.ru>
2019-07-22 11:30:38 +0300

    Add animation to url list

--- bitly2/settings.py
+++ bitly2/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_PAGINATION_CLASS': 'short_url.api.pagination.PaginationWithTotalPages',
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 5,
 }
 
 CACHES = {
